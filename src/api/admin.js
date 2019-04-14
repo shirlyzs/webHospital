479be7fb7d7830ad2bedc2cb1d9fda4ebc177c60
zerozyn <254743939@qq.com>
2019-04-14 22:56:37 +0800

    管理员相关

--- src/api/admin.js
+++ src/api/admin.js
@@ -8,3 +8,72 @@ export const AdminLogin = (adminName, password) => {
     url: `adminLoginVerify?adminName=${adminName}&password=${password}`
   })
 }
+
+// 查询管理员列表
+export const findAdmin = (pageNum, pageSize) => {
+  return axios.request({
+    method: 'get',
+    url: `/adminLoginVerify/getAdmin/${pageNum}/${pageSize}`
+  })
+}
+// 删除
+export const delAdmin = adminId => {
+  return axios.request({
+    method: 'get',
+    url: `/adminLoginVerify/admin//delete/${adminId}`
+  })
+}
+
+// 用户相关
+// 查询用户
+export const findUser = (pageNum, pageSize) => {
+  return axios.request({
+    method: 'get',
+    url: `/adminLoginVerify/getUser/${pageNum}/${pageSize}`
+  })
+}
+// 删除
+export const delUser = userId => {
+  return axios.request({
+    method: 'get',
+    url: `/userLoginVerify/user/delete/${userId}`
+  })
+}
+// 修改用户信息
+export const updateUser = (userId, password, tel, info, balance) => {
+  return axios.request({
+    method: 'get',
+    url: `/adminLoginUpdateUser/?userId=${userId}&password=${password}&tel=${tel}&info=${info}&balance=${balance}`
+  })
+}
+
+// 医生
+// 查询
+export const findDocter = (pageNum, pageSize) => {
+  return axios.request({
+    method: 'get',
+    url: `/userLoginVerify/getDoctor/${pageNum}/${pageSize}`
+  })
+}
+// 删除
+export const delDocter = doctorId => {
+  return axios.request({
+    method: 'get',
+    url: `/userLoginVerify/docter/delete/${doctorId}`
+  })
+}
+// 修改医生信息
+export const updateDocter = (
+  doctorName,
+  password,
+  resume,
+  department,
+  keShi,
+  ranks,
+  price
+) => {
+  return axios.request({
+    method: 'get',
+    url: `/adminLoginAddDoctor/?doctorName=${doctorName}&password=${password}&resume=${resume}&department=${department}&keShi=${keShi}&ranks=${ranks}$price=${price}`
+  })
+}
--- src/views/admin/admin.vue
+++ src/views/admin/admin.vue
@@ -9,61 +9,114 @@
     <div style="margin:50px">
       <el-row :gutter="60">
         <el-col :span="5">
-
-          <div class="item-control">用户管理</div>
-          <div class="item-control">医生管理</div>
-          <div class="item-control">新闻管理</div>
-          <div class="item-control">预约统计</div>
+          <div class="item-control" @click="changeType('Admin')">管理员管理</div>
+          <div class="item-control" @click="changeType('User')">用户管理</div>
+          <div class="item-control" @click="changeType('Docter')">医生管理</div>
+          <div class="item-control" @click="changeType('News')">新闻管理</div>
+          <div class="item-control" @click="changeType('Orders')">预约统计</div>
 
         </el-col>
         <el-col :span="18">
 
-          <el-table :data="tableData" style="width: 100%" border>
-            <el-table-column prop="date" label="日期" width="180">
-            </el-table-column>
-            <el-table-column prop="name" label="姓名" width="180">
-            </el-table-column>
-            <el-table-column prop="address" label="地址">
-            </el-table-column>
+          <el-table :data="tableData" style="width: 100%" height="650" fit border>
+            <!-- 管理员 -->
+            <div v-if="type == 'Admin'">
+              <el-table-column prop="adminName" label="管理员名">
+              </el-table-column>
+            </div>
+            <!-- 用户 -->
+            <div v-if="type == 'User'">
+              <el-table-column prop="userName" label="用户名">
+              </el-table-column>
+              <el-table-column prop="password" label="密码">
+              </el-table-column>
+              <el-table-column prop="info" label="备注">
+              </el-table-column>
+              <el-table-column prop="tel" label="手机号">
+              </el-table-column>
+              <el-table-column prop="balance" label="余额">
+              </el-table-column>
+            </div>
+            <!-- 医生 -->
+            <div v-if="type == 'Docter'">
+              <el-table-column prop="doctorName" label="医生名字">
+              </el-table-column>
+              <el-table-column prop="password" label="密码">
+              </el-table-column>
+              <el-table-column prop="resume" label="简介">
+              </el-table-column>
+              <el-table-column prop="department" label="主科室">
+              </el-table-column>
+              <el-table-column prop="keShi" label="分科室">
+              </el-table-column>
+              <el-table-column prop="ranks" label="职称">
+              </el-table-column>
+              <el-table-column prop="ordersNum" label="预约数">
+              </el-table-column>
+              <el-table-column prop="price" label="门诊费">
+              </el-table-column>
+              <!-- <el-table-column prop="image" label="医生照片">
+                <template slot="header" slot-scope="scope">
+                  <img :src="" alt="">
+                </template>
+              </el-table-column> -->
+            </div>
           </el-table>
-
         </el-col>
-
       </el-row>
     </div>
   </div>
 </template>
 
 <script>
+import {
+  findAdmin,
+  delAdmin,
+  findUser,
+  delUser,
+  updateUser,
+  findDocter,
+  delDocter,
+  updateDocter
+} from '@/api/admin'
 export default {
   data() {
     return {
-      tableData: [
-        {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        },
-        {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        },
-        {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        },
-        {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }
-      ]
+      tableData: [],
+      type: 'Admin'
     }
   },
-  methods: {},
-  created() {}
+  methods: {
+    changeType(type) {
+      this.type = type
+      if (type == 'User') {
+        this.getUser()
+      } else if (type == 'Docter') {
+        this.getDocter()
+      }
+    },
+    getAdmin() {
+      findAdmin(0, 20, 1).then(res => {
+        this.tableData = res.result[0].content
+      })
+    },
+    getUser() {
+      findUser(0, 20).then(res => {
+        this.tableData = res.result[0].content
+      })
+    },
+    getDocter() {
+      findDocter(0, 20).then(res => {
+        this.tableData = res.result[0].content
+        this.tableData.forEach(item => {
+          item.resume = item.resume.substring(0, 20) + '...'
+        })
+      })
+    }
+  },
+  created() {
+    this.getAdmin()
+  }
 }
 </script>
 
@@ -100,6 +153,13 @@ export default {
   margin: 10px 20px;
   border-radius: 10px;
   background: #0087cd;
+}
+.overRow {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
 }
 </style>
 
